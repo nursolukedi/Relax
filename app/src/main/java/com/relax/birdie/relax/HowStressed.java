436b3be587d67ee351cb873e0c87acfdf6c4c61c
Nursena Kal <nursena.kal@ug.bilkent.edu.tr>
2019-03-15 16:00:44 +0300

    newCode

---
+++ app/src/main/java/com/relax/birdie/relax/HowStressed.java
@@ -0,0 +1,13 @@
+package com.relax.birdie.relax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HowStressed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_stressed);
+    }
+}
--- app/src/main/java/com/relax/birdie/relax/LoginActivity.java
+++ app/src/main/java/com/relax/birdie/relax/LoginActivity.java
@@ -7,34 +7,31 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     EditText password , email ;
-    private FirebaseAuth mAuth;
+   //private FirebaseAuth mAuth;
     Button signIn, signUp;
+    ImageView relaxImage;
 
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+       // FirebaseUser currentUser = mAuth.getCurrentUser();
        // updateUI(currentUser);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
+       // mAuth = FirebaseAuth.getInstance();
         password = (EditText) findViewById(R.id.passwordText);
         email = (EditText) findViewById(R.id.mailText);
-
+        relaxImage = findViewById(R.id.relaxImage);
         signIn = findViewById(R.id.signIn);
         signUp = findViewById(R.id.signUp);
 
@@ -50,7 +47,7 @@ public class LoginActivity extends AppCompatActivity {
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                /*mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
                         .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -72,7 +69,7 @@ public class LoginActivity extends AppCompatActivity {
 
                                 // ...
                             }
-                        });
+                        });*/
             }
         });
     }
