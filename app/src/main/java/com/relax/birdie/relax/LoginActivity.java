a59b0da6aabbd54e46b0843f55b8f6116f31f67c
Nursena Kal <nursena.kal@ug.bilkent.edu.tr>
2019-03-30 14:37:52 +0300

    updated

--- app/src/main/java/com/relax/birdie/relax/LoginActivity.java
+++ app/src/main/java/com/relax/birdie/relax/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.relax.birdie.relax;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,67 +13,98 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class LoginActivity extends AppCompatActivity {
-    EditText password , email ;
-   //private FirebaseAuth mAuth;
-    Button signIn, signUp;
-    ImageView relaxImage;
+    private EditText password , email ;
+    private FirebaseAuth mAuth;
+    private Button signIn, signUp;
+    private ImageView relaxImage;
+    private ProgressDialog progressDialog;
 
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-       // FirebaseUser currentUser = mAuth.getCurrentUser();
-       // updateUI(currentUser);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-       // mAuth = FirebaseAuth.getInstance();
-        password = (EditText) findViewById(R.id.passwordText);
-        email = (EditText) findViewById(R.id.mailText);
+        mAuth = FirebaseAuth.getInstance();
+        password = findViewById(R.id.passwordText);
+        email = findViewById(R.id.mailText);
         relaxImage = findViewById(R.id.relaxImage);
         signIn = findViewById(R.id.signIn);
         signUp = findViewById(R.id.signUp);
+        relaxImage = findViewById(R.id.relaxImage);
 
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        progressDialog = new ProgressDialog(this);
 
-                // send to sign up
+        if(mAuth.getCurrentUser() != null) {
+            // start profile activity
+            System.out.println("Email of the current user: "+ mAuth.getCurrentUser().getEmail());
+            finish();
+            startActivity(new Intent(getApplicationContext(), Dashboard.class));
+        }
 
-            }
-        });
+        signUp.setOnClickListener(this);
+        signIn.setOnClickListener(this);
+    }
 
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /*mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d("login page", "signInWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    // get to dashboard.
+    public void onClick(View v) {
+        if (v == signIn) {
+            userLogin();
+        }
+        if (v == signUp) {
+            // will open registration activity here
+            finish();
+            startActivity(new Intent(this, Dashboard.class));
+        }
+    }
 
+    private void userLogin() {
+        String textEmail = email.getText().toString().trim();
+        String textPassword = password.getText().toString().trim();
 
-                                    //updateUI(user);
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w("login page", "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_LONG).show();
-                                    //updateUI(null);
-                                }
+        if (TextUtils.isEmpty(textEmail)) {
+            // Text is empty
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-                                // ...
-                            }
-                        });*/
-            }
-        });
+        if (TextUtils.isEmpty(textPassword)) {
+            // Password is empty
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // If validations are OK, it will first show a progress bar
+        progressDialog.setMessage("Logging in...");
+        progressDialog.show();
+
+        // Firebase sign in step
+        mAuth.signInWithEmailAndPassword(textEmail, textPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            // start profile activity
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), Dashboard.class));
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, "Login failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
