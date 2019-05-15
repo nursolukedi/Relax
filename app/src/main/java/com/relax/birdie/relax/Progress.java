d4a2eee77f473ff8ee4440db41f7f49ba0854719
taskafa <iserdartaskafa@gmail.com>
2019-05-15 13:28:04 +0300

    Progress class implemented

--- app/src/main/java/com/relax/birdie/relax/Progress.java
+++ app/src/main/java/com/relax/birdie/relax/Progress.java
@@ -1,14 +1,28 @@
 package com.relax.birdie.relax;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Progress extends AppCompatActivity {
-
+public class Progress extends AppCompatActivity implements View.OnClickListener{
+    private Button returnButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
+        returnButton = findViewById(R.id.buttonToDashboard);
+
+        returnButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == returnButton){
+            Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+            startActivity(intent);
+        }
     }
 
     //TODO Progress class
