1809addd876c2ac7beb4aef259e675d6d9880690
Nursena Kal <nursena.kal@ug.bilkent.edu.tr>
2019-05-17 11:02:20 +0300

    cleanup

--- app/src/main/java/com/relax/birdie/relax/Progress.java
+++ app/src/main/java/com/relax/birdie/relax/Progress.java
@@ -5,16 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class Progress extends AppCompatActivity implements View.OnClickListener{
+ public class Progress extends AppCompatActivity implements View.OnClickListener{
     private Button returnButton;
+    TextView progressText ;
+    String progressString ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
-        returnButton = findViewById(R.id.buttonToDashboard);
 
+        progressText = findViewById(R.id.progressText);
+        returnButton = findViewById(R.id.buttonToDashboard);
         returnButton.setOnClickListener(this);
+        progressString = "Date : 15-5-19 11:14, Stress : High, Meditation Tried : None \n " +
+                "\"Date : 14-5-19 13:15, Stress : High, Meditation Tried : Go outside \n " +
+                "\"Date : 14-5-19 10:43, Stress : High, Meditation Tried : Go outside \n" +
+                "\"Date : 13-5-19 09:20, Stress : Normal, Meditation Tried : None \n " +
+                "\"Date : 09-5-19 21:19, Stress : Low, Meditation Tried : Meditate \n ";
+        progressText.setText(progressString);
     }
 
     @Override
@@ -25,5 +36,5 @@ public class Progress extends AppCompatActivity implements View.OnClickListener{
         }
     }
 
-    //TODO Progress class
+
 }
